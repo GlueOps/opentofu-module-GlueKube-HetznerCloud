02a39196d3fd398612565b8459dce48ddc124130
Alanis <allanice001@users.noreply.github.com>
2026-03-08 00:29:06 +0000

    chore: Update autoglue provider version to 0.10.6 (#31)

--- modules/gluekube/provider.tf
+++ modules/gluekube/provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
     autoglue = {
       source  = "registry.terraform.io/GlueOps/autoglue"
-      version = "0.10.5"
+      version = "0.10.6"
     }
     random = {
       source  = "hashicorp/random"
