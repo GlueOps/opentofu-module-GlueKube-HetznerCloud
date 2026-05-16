e6ee2189453acfa6e20928df8fee266e0e47eef0
Venkat <venkata@venkatamutyala.com>
2026-05-15 20:57:10 -0700

    fix: missing calico_node_address_autodetection_v4
    Updated error message for network_service_cidr variable and added calico_node_address_autodetection_v4 variable with validation.

--- variables.tf
+++ variables.tf
@@ -51,11 +51,18 @@ variable "network_service_cidr" {
   type = string
   validation {
     condition     = can(cidrnetmask(var.network_service_cidr))
-    error_message = "network_service_cidr must be a valid IPv4 CIDR block for Kubernetes services, for example: 10.96.0.0/12."
+    error_message = "network_service_cidr must be a valid IPv4 CIDR block for Kubernetes services, for example: 192.168.0.0/16."
     }
 }
 
-
+variable "calico_node_address_autodetection_v4" {
+  type    = string
+  default = null
+  validation {
+    condition     = var.calico_node_address_autodetection_v4 == null || can(cidrnetmask(var.calico_node_address_autodetection_v4))
+    error_message = "calico_node_address_autodetection_v4 must be a valid IPv4 CIDR block, for example: 10.62.0.0/15."
+  }
+}
 
 variable "bastion" {
   description = "Bastion configuration."
